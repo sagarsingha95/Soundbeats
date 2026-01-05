76d033bee40e34284192fb49dd4b439a992cb27f
Sagar Singha <sagarsingha619@gmail.com>
2026-01-05 20:20:07 +0530

    phase 3

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,9 +5,19 @@ import { AiFillLike } from "react-icons/ai";
 import { MdOutlinePlaylistPlay } from "react-icons/md";
 
 const menuItems = [
-  { name: "Home", icon: <FaHome size={30}/>, path: "/" ,id:1},
-  { name: "Liked Songs", icon: <AiFillLike size={30}/>, path: "/likedsongs",id:2 },
-  { name: "Playlist", icon: <MdOutlinePlaylistPlay size={30}/>, path: "/playlist",id:3 },
+  { name: "Home", icon: <FaHome size={30} />, path: "/", id: 1 },
+  {
+    name: "Liked Songs",
+    icon: <AiFillLike size={30} />,
+    path: "/likedsongs",
+    id: 2,
+  },
+  {
+    name: "Playlist",
+    icon: <MdOutlinePlaylistPlay size={30} />,
+    path: "/playlist",
+    id: 3,
+  },
 ];
 
 const SideBar = () => {
@@ -20,8 +30,8 @@ const SideBar = () => {
         {/* Logo */}
         <div className="flex items-center justify-center mb-6">
           <h1 className="text-[20px] font-bold flex items-center gap-2  bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-transparent">
-            <FaMusic color="green"/> Sound Beats
-          </h1> 
+            <FaMusic color="green" /> Sound Beats
+          </h1>
         </div>
 
         {/* Menu */}
@@ -50,22 +60,27 @@ const SideBar = () => {
       </div>
 
       {/* Mobile Top Nav */}
-      <div className="md:hidden fixed bottom-0 left-0 w-full bg-[#121212] text-white flex justify-around p-2 z-50 border-b border-white/10">
-        {menuItems.map((item) => {
-          const isActive = location.pathname === item.path;
-          return (
-            <Link
-              key={item.name}
-              to={item.path}
-              className={`flex flex-col items-center text-xs ${
-                isActive ? "text-green-500" : "text-white/70"
-              }`}
-            >
-              <span className="text-lg">{item.icon}</span>
-              <span>{item.name}</span>
-            </Link>
-          );
-        })}
+      <div className="md:hidden sticky top-0 w-full bg-[#121212] text-white flex justify-around p-2 z-50 border-b border-white/10  flex-col ">
+        <h1 className="text-[20px] font-bold flex m-auto mb- gap-2 pb-4 !text-4xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-transparent">
+          <FaMusic color="green" /> Sound Beats
+        </h1>
+        <div className="flex justify-around">
+          {menuItems.map((item) => {
+            const isActive = location.pathname === item.path;
+            return (
+              <Link
+                key={item.name}
+                to={item.path}
+                className={`flex flex-col items-center text-xs ${
+                  isActive ? "text-green-500" : "text-white/70"
+                }`}
+              >
+                <span className="text-lg">{item.icon}</span>
+                <span>{item.name}</span>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </>
   );
